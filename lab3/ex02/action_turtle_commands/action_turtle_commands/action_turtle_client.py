0fd90b85307dabe0941e89049d67fa762970ed25
KadilenkoIvan <kadilenkoivan@gmail.com>
2024-10-02 13:53:58 +0700

    1

--- lab3/ex02/action_turtle_commands/action_turtle_commands/action_turtle_client.py
+++ lab3/ex02/action_turtle_commands/action_turtle_commands/action_turtle_client.py
@@ -9,26 +9,31 @@ class ActionTurtleClient(Node):
         super().__init__('action_turtle_client')
         self._action_client = ActionClient(self, MessageTurtleCommands, 'move_turtle')
 
-    def send_goal(self, command, s, angle):
+    def send_goal(self, commands_list):
         goal_msg = MessageTurtleCommands.Goal()
-        goal_msg.command = command
-        goal_msg.s = s
-        goal_msg.angle = angle
-
+        for command in commands_list:
+            goal_msg.command = command[0]
+            goal_msg.s = command[1]
+            goal_msg.angle = command[2]
+            future = self._action_client.send_goal(goal_msg)
+            rclpy.spin_until_future_complete(self, future)
         #self._action_client.wait_for_server()
-        return self._action_client.send_goal(goal_msg)
+        return 1
 
 def main(args=None):
     rclpy.init(args=args)
     action_turtle_client = ActionTurtleClient()
 
-    #action_turtle_client.send_goal("forward", 2, 0)  # проехать вперед 2 метра
-    #time.sleep(5)
-    action_turtle_client.send_goal("turn", 90, 0)  # повернуть направо на 90 градусов
-    action_turtle_client.get_logger().info("turn")
-    time.sleep(5)
-    action_turtle_client.send_goal("forward", 1, 0)  # проехать еще 1 метр
-    action_turtle_client.get_logger().info("second")
+    command_list = [["forward", 2, 0], ["turn", 90, 0], ["forward", 1, 0]]
+    action_turtle_client.send_goal(command_list)
+
+#    action_turtle_client.send_goal("forward", 2, 0)  # проехать вперед 2 метра
+#    time.sleep(5)
+#    action_turtle_client.send_goal("turn", 90, 0)  # повернуть направо на 90 градусов
+#    action_turtle_client.get_logger().info("turn")
+#    time.sleep(5)
+#    action_turtle_client.send_goal("forward", 1, 0)  # проехать еще 1 метр
+#    action_turtle_client.get_logger().info("second")
 
     rclpy.spin(action_turtle_client)
     action_turtle_client.destroy_node()
