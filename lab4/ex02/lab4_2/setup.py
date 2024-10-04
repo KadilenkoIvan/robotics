11bc694c2792f8b51ee8db2c4d09a624e806a40e
KadilenkoIvan <kadilenkoivan@gmail.com>
2024-10-04 18:29:59 +0700

    fixed lab4_2

--- lab4/ex02/lab4_2/setup.py
+++ lab4/ex02/lab4_2/setup.py
@@ -1,7 +1,8 @@
 from setuptools import find_packages, setup
 import os
 from glob import glob
-package_name = 'lab4_2_rad'
+
+package_name = 'lab4_2'
 
 setup(
     name=package_name,
@@ -10,24 +11,24 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name,
+        ('share/' + package_name, 
             ['package.xml']),
-        (os.path.join('share', package_name, 'launch'),
-             glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'launch'), 
+            glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ivan',
     maintainer_email='kadilenkoivan@gmail.com',
     description='TODO: Package description',
-    license='TODO: License declaration',
+    license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'static_broadcaster = lab4_2_rad.static_broadcaster:main',
-            'carrot_broadcaster = lab4_2_rad.carrot_broadcaster:main',
-            'turtle_follower = lab4_2_rad.turtle_follower:main',
-            'turtle_spawner = lab4_2_rad.turtle_spawner:main',
+            'static_turtle_tf2_broadcaster = lab4_2.static_turtle_tf2_broadcaster:main',
+	    'turtle_tf2_broadcaster = lab4_2.turtle_tf2_broadcaster:main',
+            'turtle_tf2_listener = lab4_2.turtle_tf2_listener:main',
+            'fixed_frame_tf2_broadcaster = lab4_2.fixed_frame_tf2_broadcaster:main',
         ],
     },
 )
